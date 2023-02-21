cdee0f683d78d424f615a3187e24e7863c925e32
BerberKagan <kaganberbero@gmail.com>
2023-02-21 18:27:08 +0300

    Checked off my finished tasks

--- src/index.js
+++ src/index.js
@@ -43,3 +43,37 @@ console.log('Proje açıldı!')
 
 
 /* Kodlar Buradan aşağıya */
+const baslikListe = document.querySelectorAll('nav a');
+
+/* baslikListe[0].textContent = siteContent.nav["nav-item-1"];
+baslikListe[1].textContent = siteContent.nav["nav-item-2"];
+baslikListe[2].textContent = siteContent.nav["nav-item-3"];
+baslikListe[3].textContent = siteContent.nav["nav-item-4"];
+baslikListe[4].textContent = siteContent.nav["nav-item-5"];
+baslikListe[5].textContent = siteContent.nav["nav-item-6"]; */
+
+const navArray = baslikListe.forEach((element, index) => {
+  element.textContent = siteContent.nav[`nav-item-${index + 1}`];
+  baslikListe[index].setAttribute('class', 'italic');
+});
+
+const footerListe = document.querySelectorAll('footer a');
+for (let index = 0; index<footerListe.length; index++) {
+  footerListe[index].textContent = siteContent.footer.copyright;
+  footerListe[index].setAttribute('class', 'bold');
+}
+
+const image1 = document.querySelector('.logo');
+image1.setAttribute('src', 'http://localhost:9000/img/logo.png');
+
+const image2 = document.querySelector('#cta-img');
+image2.setAttribute('src', 'http://localhost:9000/img/cta.png');
+
+const image3 = document.querySelector('.middle-img');
+image3.setAttribute('src', 'http://localhost:9000/img/accent.png');
+
+const ctaTexth1 = document.querySelector('.cta-text h1');
+ctaTexth1.textContent = siteContent.cta.h1;
+
+const button = document.querySelector('.cta-text button');
+button.textContent = siteContent.cta.button;
